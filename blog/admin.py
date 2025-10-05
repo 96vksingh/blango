f9fbeae50314f8f3b57d06924acfcf1da4afc9e2
Raj Singh <raj24singh99@gmail.com>
2025-10-05 15:58:07 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from blog.models import Tag, Post
-# Register your models here.
-admin.site.register(Tag)
+from .models import Tag, Post, Comment
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('slug','published_at')
+    list_display = ("title", "slug", "published_at")
+    list_filter = ("published_at", "tags")
+    search_fields = ("title", "summary", "content")
+    autocomplete_fields = ("tags",)
 
-
-admin.site.register(Post, PostAdmin)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("creator", "content_type", "object_id", "created_at")
+    list_select_related = ("content_type",)
+    search_fields = ("content",)
--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,42 @@
 from django.db import models
 from django.conf import settings
-# Create your models here.
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
 class Tag(models.Model):
-    value = models.TextField(max_length=100)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.value
+        return self.name
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    # Generic FK parts
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.creator} on {self.created_at:%Y-%m-%d}"
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(blank=True, null=True)
-    title = models.TextField(max_length=100)
-    slug = models.SlugField()
-    summary = models.TextField(max_length=500)
+
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    summary = models.CharField(max_length=500)
     content = models.TextField()
-    tags = models.ManyToManyField(Tag, related_name="posts")
+
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
+    comments = GenericRelation(Comment, related_query_name="post")
 
     def __str__(self):
         return self.title
